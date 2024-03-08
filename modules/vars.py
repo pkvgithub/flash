60e326bf5907df6277f1be80a49e11ee38836103
Krishna00008 <156284133+Krishna00008@users.noreply.github.com>
2024-03-08 22:10:36 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "23348929"
 api_hash = "fcf48c30db94f64e130fd482a367fd91"
-bot_token = "6689478782:AAHaMgNDDtGYURbqYLQfdtafdVyNT0JMEb0"
+bot_token = "7086212221:AAHwc0EkrnxmR1XBpQel2V03cB35c0665XA"
